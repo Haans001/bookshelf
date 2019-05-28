33f2573c995dcbd07bea172f688299161f211db0
Jan Rapacz <xperiamfan@gmail.com>
2019-05-28 15:17:10 +0200

    Project configuration

---
+++ client/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    document: true,
+    foo: true,
+    window: true,
+    navigator: true,
+    it: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  parser: 'babel-eslint',
+  plugins: ['react', 'prettier', 'jsx-a11y', 'import'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};
